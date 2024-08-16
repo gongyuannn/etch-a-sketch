e85314746667cb8514af4868794bdddbd8def4ef
GY <104050074+gongyuannn@users.noreply.github.com>
2024-08-16 09:37:59 +0800

    Add mouseover event listener to grid squares for color change upon hovering

--- myscript.js
+++ myscript.js
@@ -1,12 +1,15 @@
 // Create 16 x 16 grid
-
 const container = document.querySelector(".grid");
 for(let i = 0; i < 16*16; i++){
     const gridSquare = document.createElement("div");
     container.appendChild(gridSquare);
+    // Add the mouseover event listener to each grid square. Use 'add' instead of 'toggle' (on/off effect). 
+    gridSquare.addEventListener("mouseover", () =>    gridSquare.classList.add("hover"));
 }
-// incl. Borders and margins
-// Set up hover effect - so grid div change colour when mouse passes
+
+
+
+
 // Add button on the screen. 
     // button sends pop up to user asking for no. of squares per side (1-100)
     // it should be in the same space
