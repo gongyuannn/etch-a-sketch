8e1b69c521b3fffbf054e9b2a21f7082edbec0a5
GY <104050074+gongyuannn@users.noreply.github.com>
2024-08-18 21:38:54 +0800

    Fix size of grid squares to fit into grid container

--- myscript.js
+++ myscript.js
@@ -1,15 +1,43 @@
-// Create 16 x 16 grid
 const container = document.querySelector(".grid");
-for(let i = 0; i < 16*16; i++){
-    const gridSquare = document.createElement("div");
-    container.appendChild(gridSquare);
-    // Add the mouseover event listener to each grid square. Use 'add' instead of 'toggle' (on/off effect). 
-    gridSquare.addEventListener("mouseover", () =>    gridSquare.classList.add("hover"));
+const button = document.querySelector("#generateGrid");
+
+// Create function to generate grid squares
+function generateGrid(gridSize) {
+    
+    //Remove existing grid squares
+    while (container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+
+    // Create new grid
+    for(let i = 0; i < gridSize*gridSize; i++){
+        const gridSquare = document.createElement("div");
+        gridSquare.style.width = `calc(100% /${gridSize})`;
+        gridSquare.style.height = `calc(100% /${gridSize})`;
+        container.appendChild(gridSquare);
+      
+        gridSquare.addEventListener("mouseover", () => gridSquare.classList.add("hover"));
+    }
 }
 
+//Create default 16x 16 grid
+generateGrid(16);
+
+//Click button to prompt for user input
+button.addEventListener("click", () => {
+
+    let gridSize = prompt("Enter the grid size (whole no. from 1-100):");
+
+    gridSize = Number(gridSize);
+
+    if(isNaN(gridSize) || !Number.isInteger(gridSize) || gridSize < 1 || gridSize > 100) {
+        alert("Please enter a valid whole no. between 1-100");
+        return; //stop function from running
+    }
+
+    //Generate grid based on user input
+    generateGrid(gridSize);
+
+});
 
 
-
-// Add button on the screen. 
-    // button sends pop up to user asking for no. of squares per side (1-100)
-    // it should be in the same space
