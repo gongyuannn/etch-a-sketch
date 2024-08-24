9c0510426708d9b274273427e17b21004632de7b
GY <104050074+gongyuannn@users.noreply.github.com>
2024-08-24 08:08:43 +0800

    Add gradient and colorful color schemes

--- myscript.js
+++ myscript.js
@@ -1,9 +1,12 @@
-//TO-DO: Optional colorful
-//TO-DO: Optional gradient
+// - for gradient - if it is already fully opaque, restart at 1
+// - push to main branch
+// fix bottom line for smaller screens (disappearing)
 
 const container = document.querySelector(".grid");
 const newButton = document.querySelector("#generateGrid");
 const resetButton = document.querySelector("#resetGrid");
+const colorSelection = document.querySelector("#colorScheme");
+let colorScheme = "black";
 
 //Create default 16x 16 grid
 let gridSize = 16; 
@@ -24,11 +27,49 @@ function generateGrid(gridSize) {
         gridSquare.style.height = `calc(100% /${gridSize})`;
         container.appendChild(gridSquare);
       
-        gridSquare.addEventListener("mouseover", () => gridSquare.classList.add("hover"));
+        gridSquare.addEventListener("mouseover", () => applyColorScheme(gridSquare));
     }
 }
 
-//Click newButton to prompt for user input
+//Create function to generate random RGB value
+function randomInteger(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+function getRandomColor(){
+    let r = randomInteger(255);
+    let g = randomInteger(255);
+    let b = randomInteger(255);
+    return `rgb(${r},${g},${b})`;
+}
+
+function applyColorScheme(gridSquare){
+    switch(colorScheme){
+        case "black":
+            gridSquare.style.backgroundColor = "rgb(4, 4, 4)";
+            gridSquare.style.opacity = 1;
+            break;
+        case "gradient":
+            if (gridSquare.style.backgroundColor === "rgb(4, 4, 4)" && gridSquare.style.opacity === "1"){
+                let currentOpacity = 0.1;
+                // Assign new opacity, up to 1
+                gridSquare.style.opacity = Math.min(currentOpacity + 0.1, 1);
+            } else if (gridSquare.style.opacity > 0 && gridSquare.style.opacity < 1){
+                currentOpacity = parseFloat(gridSquare.style.opacity);
+                gridSquare.style.opacity = Math.min(currentOpacity + 0.1, 1);
+            } else {
+                gridSquare.style.backgroundColor = "rgb(4, 4, 4)";
+                gridSquare.style.opacity = "0.1";
+            };
+            break;
+        case "colorful":
+            gridSquare.style.backgroundColor = getRandomColor();
+            gridSquare.style.opacity = 1;
+            break;
+    }
+}
+
+//Event listener to prompt for user input
 newButton.addEventListener("click", () => {
     //Prompt until user gives valid input
     
@@ -48,6 +89,12 @@ newButton.addEventListener("click", () => {
 
 });
 
+//Event listener for dropdown change
+colorSelection.addEventListener("change", (event) => {
+    colorScheme = event.target.value;
+});
+
+//Event listener for reset
 resetButton.addEventListener("click", ()=>
     generateGrid(gridSize)
 )
